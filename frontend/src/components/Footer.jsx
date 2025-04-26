4466e681de8ca98587215246bfd4f238c4aadbd4
keerthiboga <keerthiboga2004@gmail.com>
2025-04-26 10:02:25 +0530

    tested few files

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-blue-400 rounded-lg shadow-sm m-4 dark:bg-gray-800'>Footer</div>
+  )
+}
+
+export default Footer
